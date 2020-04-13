ba6b850037be8c678c25eab48cb578c7fe5222a8
1995hyz <1995hyz@gmail.com>
2020-04-13 13:43:54 -0400

    2nd Commit

--- project_2/CKY_parser.py
+++ project_2/CKY_parser.py
@@ -16,6 +16,7 @@ def load_cnf(file_path):
                 grammar.update({key: [left]})
             else:
                 grammar[key].append(left)
+    return grammar
 
 
 def cky_parse(words, grammar):
@@ -26,8 +27,21 @@ def cky_parse(words, grammar):
         word = words[j-1]
         if word in grammar:
             for rule in grammar[word]:
-                cell = TableCell(rule, cell_type=0, comp1=(word, 0, 0), comp2=(None, 0, 0))
+                cell = TableCell(prod=rule, cell_type=0, comp1=(word, 0, 0), comp2=(None, 0, 0))
                 table[j-1][j].append(cell)
+        else:
+            return None
         for i in range(j-2, -1, -1):
             for k in range(i+1, j, 1):
-                pass
+                for cell_b in table[i][k]:
+                    for cell_c in table[k][j]:
+                        prod = cell_b.prod + "$" + cell_c.prod
+                        if prod in grammar:
+                            cell = TableCell(prod=prod, cell_type=1, comp1=(cell_b.prod, i, k), comp2=(cell_c.prod, k, j))
+                            table[i][j].append(cell)
+        return table
+
+
+if __name__ == "__main__":
+    cnf_path = "C:\\Users\\1995h\\PycharmProjects\\NLP_projects\\cnf_rules.txt"
+    cnf_grammar = load_cnf(cnf_path)
